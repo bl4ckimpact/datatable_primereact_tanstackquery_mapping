ee355cac578ce870cf5cc0c43399c862cbad1015
bl4ckimpact <bl4ckimpact@gmail.com>
2024-03-24 07:25:07 +0700

    pokemon

--- src/app/datatable/page.tsx
+++ src/app/datatable/page.tsx
@@ -12,12 +12,7 @@ type poolData = {
   namapaket_konstruksi: string;
 };
 
-interface Price {
-  name: string;
-  symbol: string;
-}
-
-const getMarket = async () => {
+const getPokemon = async () => {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10", {
     next: { revalidate: 0 },
   });
@@ -26,37 +21,22 @@ const getMarket = async () => {
 };
 
 const Page = () => {
-  //console.log(datapost);
-  const { data, isError, isFetching, isLoading, isSuccess } = useQuery({
-    queryKey: ["admpembangunantkn"],
-    queryFn: getMarket,
+  const { data } = useQuery({
+    queryKey: ["PokemonData"],
+    queryFn: getPokemon,
   });
-
-  const [post, setPosts] = useState([]);
-
-  useEffect(() => {
-    getMarket().then((data) => setPosts(data));
-  }, []);
 
   return (
     <>
       <ReactQueryDevtools initialIsOpen={false} />
 
       <div className="m-10rem p-10rem card">
-        {/* {data.map((kumpulanData: any) => (
-          <>{kumpulanData.id}</>
-        ))} */}
-
         {data?.results?.map(({ kumpulanData }: any) => (
           <DataTable value={kumpulanData}>
             <Column field="name" header="name"></Column>
             <Column field="url" header="url"></Column>
           </DataTable>
         ))}
-
-        {/* {data?.map((price: Price) => (
-          <>{price.id}</>
-        ))} */}
       </div>
     </>
   );
